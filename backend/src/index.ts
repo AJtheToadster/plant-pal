ba3df77fee5de11fd72636b8af4d4e26f31d8704
AJ <ajthetoadster@gmail.com>
2026-08-02 20:58:44 +0000

    Added POST /plants

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,6 +17,23 @@ app.get('/api/plants', async (req, res) => {
     res.json(plants)
 });
 
+app.post('/api/plants', async (req, res) => {
+    const plantName = req.body.name
+    const plantSpecies = req.body.species
+    if (!plantName) {
+        return res.status(400).json({
+            error: 'Plant name is required'
+        })
+    }
+    const newPlant = await prisma.plant.create({
+        data: {
+            name: plantName,
+            species: plantSpecies
+        }
+    })
+    return res.status(201).json(newPlant)
+});
+
 app.listen(PORT, () => {
     console.log(`🚀 Server listening on http://localhost:${PORT}`);
 });
